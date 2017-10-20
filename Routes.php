81d81c547e8c73b97c21ef8e64546559f051dbe3
Khomotjo MODIPA <kmodipa@e4r13p2.wethinkcode.co.za>
2017-10-20 11:11:19 +0200

    our routes file, all routes gets handled here

---
+++ Routes.php
@@ -0,0 +1,10 @@
+<?php
+
+/* This file helps you to access routes */
+
+Route::set('about-us', function () {
+    /* When we access a route we can run a function here */
+    echo "about-us";
+});
+
+?>
