e3d0c09573661d2f0b51e4fadc5ac383a6dbd456
Khomotjo MODIPA <kmodipa@e4r13p2.wethinkcode.co.za>
2017-10-20 17:05:51 +0200

    added a route for index page

--- Routes.php
+++ Routes.php
@@ -2,13 +2,17 @@
 
 /* This file helps you to access routes */
 
+Route::set('index', function () {
+    Index::CreatView('Index');
+});
+
 Route::set('about-us', function () {
     /* When we access a route we can run a function here */
-    echo "about-us";
+    AboutUs::CreatView('AboutUs');
 });
 
 Route::set('contact-us', function () {
-    echo "contact-us";
-})
+    ContactUs::CreatView('ContactUs');
+});
 
 ?>
