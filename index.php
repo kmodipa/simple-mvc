fa1c9d55f73b54cfa19488f7c4a1ac1af9e22dfa
Khomotjo MODIPA <kmodipa@e4r13p2.wethinkcode.co.za>
2017-10-20 12:15:56 +0200

    added comments

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
 
-/* 1st things 1st we include our routes */
+/* 1st things 1st we include our routes
+ *
+ * By including Routes.php we get access to the $Routes
+ * array containing all of the valid routes for our app
+ */
 require_once ('Routes.php');
 
 
-/*  */
+/* The first thing we do is autoload all of our classes. We're using the older
+ * __autoload function as opposed to spl_autoload_register because all of our
+ * classes are located in the /classes/ directory.
+ */
 function __autoload($class_name)
 {
     require_once './classes/'.$class_name.'.php';
