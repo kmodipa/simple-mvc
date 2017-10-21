33d377f83fc1a9135487b62af2cee39bd734917c
Khomotjo MODIPA <kmodipa@e4r13p2.wethinkcode.co.za>
2017-10-21 19:23:30 +0200

    database class

--- classes/Database.php
+++ classes/Database.php
@@ -2,14 +2,28 @@
 
 class Database
 {
+    public static $host = "127.0.0.1";
+    public static $dbName = "example";
+    public static $username = "root";
+    public static $password = "PFypuTEL";
+
+    /* The static just means we do not need to creat an object of the db-class in oreder to use it */
     private static function connect()
     {
-
+        $pdo = new PDO("mysql:host=".self::$host.";dbname=".self::$dbName.";charset=utf8", self::$username, self::$password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
     }
 
-    public static function query()
+    public static function query($query, $params = array())
     {
-
+        $statement = self::connect()->prepare($query);
+        $statement->execute($params);
+        if (explode(' ',$query)[0] == 'SELECT')
+        {
+            $data = $statement->fetchAll();
+            return $data;
+        }
     }
 }
 
