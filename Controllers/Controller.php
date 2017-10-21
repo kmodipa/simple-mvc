2a7df8c0cd96f1fd6a1e882f9977d9253fc7ca7b
Khomotjo MODIPA <kmodipa@e4r13p2.wethinkcode.co.za>
2017-10-21 19:24:46 +0200

    declared doSomething()

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -1,10 +1,11 @@
 <?php
 
-class Controller
+class Controller extends Database
 {
     public static function CreatView($viewName)
     {
         require_once ("./Views/$viewName.php");
+        static::doSomething();
     }
 }
 
