e161a75278f74b74e9e901a696ace0ce77b56d81
Khomotjo MODIPA <kmodipa@e4r13p2.wethinkcode.co.za>
2017-10-22 06:00:50 +0200

    addition of login-register controller

---
+++ Controllers/login.php
@@ -0,0 +1,14 @@
+<?php
+
+	//include 'dbh.php';  /* link to database handler */
+class Login extends Controller
+{
+    if ($_POST['register'] == "OK") /* redirect to registration page */
+        header ("Location: ./Views/user_register.php");
+}
+
+if ($_POST['register'] == "OK") /* redirect to registration page */
+    header ("Location: ./Views/user_register.php")
+
+
+?>
---
+++ Controllers/register.php
@@ -0,0 +1,6 @@
+<?php
+	class Register extends Controller
+    {
+        echo "Success!";
+    }
+?>
