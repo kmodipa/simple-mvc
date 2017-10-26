bd68f9664f97d66b199f85377c5783bb577f3cc3
Khomotjo MODIPA <kmodipa@e4r13p2.wethinkcode.co.za>
2017-10-26 16:41:45 +0200

    fixed the linking problem

--- Controllers/register.php
+++ Controllers/register.php
@@ -1,6 +1,12 @@
 <?php
 	class Register extends Controller
     {
-
+			public static function create()
+			{
+				if (isset($_POST['register']) && isset($_POST['passwd']))
+				{
+					header('Location: index'); /* This is an example, remove it */
+				}
+			}
     }
 ?>
