8848cda3a76cbaf3d88645414164973476440f59
Khomotjo MODIPA <kmodipa@e4r13p2.wethinkcode.co.za>
2017-10-26 11:44:46 +0200

    added comments

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -4,8 +4,8 @@ class Controller extends Database
 {
     public static function CreatView($viewName)
     {
-        require_once ("./Views/$viewName.php");
-        static::doSomething();
+        require_once ("./Views/$viewName.php");  /* Load the view */
+        //static::doSomething();
     }
 }
 
