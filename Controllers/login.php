7ec520b409aba1c4388a959b2aae8665df69d7dc
Khomotjo MODIPA <kmodipa@e4r13p2.wethinkcode.co.za>
2017-10-26 13:49:05 +0200

    linked login-register buttons

--- Controllers/login.php
+++ Controllers/login.php
@@ -3,12 +3,6 @@
 	//include 'dbh.php';  /* link to database handler */
 class Login extends Controller
 {
-    if ($_POST['register'] == "OK") /* redirect to registration page */
-        header ("Location: ./Views/user_register.php");
+
 }
-
-if ($_POST['register'] == "OK") /* redirect to registration page */
-    header ("Location: ./Views/user_register.php")
-
-
 ?>
--- Controllers/register.php
+++ Controllers/register.php
@@ -1,6 +1,6 @@
 <?php
 	class Register extends Controller
     {
-        echo "Success!";
+
     }
 ?>
