9b47cd058e9abf60c6de636a226e878b0874970a
Khomotjo MODIPA <kmodipa@e4r13p2.wethinkcode.co.za>
2017-10-31 11:53:07 +0200

    password recovery controller

---
+++ Controllers/recovery.php
@@ -0,0 +1,11 @@
+<?php
+
+	//include 'dbh.php';  /* link to database handler */
+  class Recovery extends Controller
+  {
+    public static function recover()
+    {
+      # code...
+    }
+  }
+?>
