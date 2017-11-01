a5e48b8f284091a94467c692389210d3ce8e2c91
Khomotjo MODIPA <kmodipa@e4r13p2.wethinkcode.co.za>
2017-11-01 11:59:12 +0200

    started the register class

--- Controllers/register.php
+++ Controllers/register.php
@@ -5,7 +5,13 @@
 			{
 				if (isset($_POST['register']) && isset($_POST['passwd']))
 				{
-					header('Location: index'); /* This is an example, remove it */
+					$first_name = $_POST['first_name'];
+					$last_name = $_POST['last_name'];
+					$uid = $_POST['login'];
+					$password = hash('whirlpool', ($_POST['passwd']));
+
+					Database::query('INSERT INTO users VALUES (:first_name, :last_name, :uid, :passwd)', array(':first_name'=>$first_name, ':last_name'=>$last_name, ':uid'=>$uid, ':passwd'=>$password));
+					//header('Location: index'); /* This is an example, remove it */
 				}
 			}
     }
