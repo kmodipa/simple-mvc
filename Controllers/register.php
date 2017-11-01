50e17d50665f71b1f37f517fe7396dcf4a2abde5
Khomotjo MODIPA <kmodipa@e4r13p2.wethinkcode.co.za>
2017-11-01 15:25:57 +0200

    checked for existing user

--- Controllers/register.php
+++ Controllers/register.php
@@ -8,9 +8,18 @@
 					$first_name = $_POST['first_name'];
 					$last_name = $_POST['last_name'];
 					$uid = $_POST['login'];
+					$email = $_POST['email'];
 					$password = hash('whirlpool', ($_POST['passwd']));
 
-					Database::query('INSERT INTO users VALUES (:first_name, :last_name, :uid, :passwd)', array(':first_name'=>$first_name, ':last_name'=>$last_name, ':uid'=>$uid, ':passwd'=>$password));
+					if (!Database::query('SELECT uid FROM users WHERE uid=:uid', array(':uid'=>$uid)))
+					{
+						Database::query('INSERT INTO users VALUES (\'\', :first_name, :last_name, :uid, :email, :password)', array(':first_name'=>$first_name, ':last_name'=>$last_name, ':uid'=>$uid, ':email'=>$email, ':password'=>$password));
+						echo "Registered Successfully!";
+					}
+					else {
+						echo "User already exists!";
+					}
+
 					//header('Location: index'); /* This is an example, remove it */
 				}
 			}
