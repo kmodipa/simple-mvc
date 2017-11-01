2137d8656425c7c127a05487e06a396b7364a40a
Khomotjo MODIPA <kmodipa@e4r13p2.wethinkcode.co.za>
2017-11-01 17:00:59 +0200

    instatiated the login class

--- Controllers/login.php
+++ Controllers/login.php
@@ -1,8 +1,22 @@
 <?php
+  class Login extends Controller
+  {
+    public static function login_user()
+    {
+      if (isset($_POST['signin']))
+      {
+        $uid = $_POST['login'];
+        $password = $_POST['passwd'];
 
-	//include 'dbh.php';  /* link to database handler */
-class Login extends Controller
-{
-
-}
+        if (Database::query('SELECT uid FROM users WHERE uid=:uid', array(':uid'=>$uid)))
+        {
+          echo "User registered";
+          header('Location: index');
+        }
+        else {
+          echo "User not registered";
+        }
+      }
+    }
+  }
 ?>
