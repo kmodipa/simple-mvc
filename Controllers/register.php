a2412d797375ccd0f5c62385bf57e56c86f98e5f
Khomotjo MODIPA <kmodipa@e4r13p2.wethinkcode.co.za>
2017-11-01 15:51:21 +0200

    validation complete

--- Controllers/register.php
+++ Controllers/register.php
@@ -9,12 +9,44 @@
 					$last_name = $_POST['last_name'];
 					$uid = $_POST['login'];
 					$email = $_POST['email'];
-					$password = hash('whirlpool', ($_POST['passwd']));
+					$password = $_POST['passwd'];
 
 					if (!Database::query('SELECT uid FROM users WHERE uid=:uid', array(':uid'=>$uid)))
 					{
-						Database::query('INSERT INTO users VALUES (\'\', :first_name, :last_name, :uid, :email, :password)', array(':first_name'=>$first_name, ':last_name'=>$last_name, ':uid'=>$uid, ':email'=>$email, ':password'=>$password));
-						echo "Registered Successfully!";
+						if (strlen($uid) >= 3 && strlen($uid) <= 32)
+						{
+							if (preg_match('/[a-zA-Z0-9_]+/', $uid))
+							{
+								if (strlen($password) >= 6 && strlen($password) <= 100)
+								{
+									$password = hash('whirlpool', ($_POST['passwd'])); /* hash */
+									if (filter_var($email, FILTER_VALIDATE_EMAIL))
+									{
+										if (!self::query('SELECT email FROM users WHERE email=:email', array(':email'=>$email)))
+										{
+											Database::query('INSERT INTO users VALUES (\'\', :first_name, :last_name, :uid, :email, :password)', array(':first_name'=>$first_name, ':last_name'=>$last_name, ':uid'=>$uid, ':email'=>$email, ':password'=>$password));
+											echo "Registered Successfully!";
+											header('Location: index');
+										}
+										else{
+											echo 'Email in use!';
+										}
+									}
+									else {
+										echo 'Invalid email!';
+									}
+								}
+								else {
+									echo "Invalid Passowrd!";
+								}
+							}
+							else {
+								echo 'Invalid user';
+							}
+						}
+						else {
+							echo 'Invalid user';
+						}
 					}
 					else {
 						echo "User already exists!";
